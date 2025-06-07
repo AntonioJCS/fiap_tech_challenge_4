d326c80c4d1af72f217cf8fa63e97a9a47c05a9b
Antonio Juan Carvalho Silva <antonioxp.8@hotmail.com>
2025-06-06 23:05:06 -0300

    testes na arquitetura

--- src/app/core/database.py
+++ src/app/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-from config import DATABASE_DIR, SQLALCHEMY_DATABASE_URL
+from src.config import DATABASE_DIR, SQLALCHEMY_DATABASE_URL
 
 
 # 1. Conexão com o banco
